afe838ae8f845d9b4c16c9faca4d42e1805452a0
chiragrohit <chiragrohit812@gmail.com>
2024-02-01 17:38:01 +0530

    sveltekit skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="p-5 grid justify-items-end">
+	<div>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+	</div>
+</nav>
+
+<main class="p-5">
+	<slot />
+</main>
