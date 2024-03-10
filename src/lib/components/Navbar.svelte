3208a6fb46e9faaf0a0b1e4affdc682dc922c683
chiragrohit <chiragrohit812@gmail.com>
2024-03-10 18:04:19 +0530

    theme store added

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <script>
 	let isOpen = false;
 	import { page } from '$app/stores';
+	import { theme } from '$lib/stores';
 
 	const navbarItems = {
 		Home: '/',
@@ -11,29 +12,33 @@
 		Contact: '/contact',
 		Test: '/test'
 	};
+
+	function changeTheme() {
+		$theme = !$theme;
+	}
 </script>
 
 <!-- {JSON.stringify($page)} -->
+Theme: {$theme}
 <nav
 	class="text-white shadow-lg sticky top-0 z-50 bg-gradient-to-r from-emerald-900 to-emerald-500"
 >
 	<div class="mx-auto max-w-screen-xl">
 		<div class="flex justify-between">
 			<div class="flex justify-between items-center w-full">
-				<!-- Left side: logo and primary nav -->
+				<!-- logo -->
+				<div class="hover:text-emerald-300">
+					<a href="/" class="flex items-center py-5 px-3">
+						<span class="text-xl font-bold">Chirag Rohit</span>
+					</a>
+				</div>
 				<div class="flex space-x-4">
-					<!-- logo -->
-					<div class="hover:text-emerald-300">
-						<a href="/" class="flex items-center py-5 px-3">
-							<span class="text-xl font-bold">Chirag Rohit</span>
-						</a>
-					</div>
-
 					<!-- primary nav -->
 					<div class="hidden md:flex items-center space-x-1">
 						{#each Object.entries(navbarItems) as [label, route]}
 							<a href={route} class="py-2 px-3 hover:bg-emerald-800 hover:text-white">{label}</a>
 						{/each}
+						<button on:click={changeTheme} class="btn-primary">Theme</button>
 					</div>
 				</div>
 			</div>
